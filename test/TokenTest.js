0f7b8d7ada537363a85b68c12e1573c73fe084ff
matheus.custodio <matheus.c.conceicao@protonmail.com>
2021-12-29 12:20:40 -0300

    Tests
    Tests

--- test/TokenTest.js
+++ test/TokenTest.js
@@ -2,11 +2,11 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Token contract", function () {
-  let Token, token, owner, addr1, addr2;
+  let Token, token, owner;
   beforeEach(async () => {
     Token = await ethers.getContractFactory("Token");
     token = await Token.deploy();
-    [owner, addr1, addr2] = await ethers.getSigners();
+    [owner] = await ethers.getSigners();
   });
   it("Deployment should assign the total supply of tokens to the owner", async function () {
     const ownerBalance = await token.balanceOf(owner.address);
--- test/WalletTest.js
+++ test/WalletTest.js
@@ -1,24 +1,26 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
 
-describe("Wallet contract", () => {
-  let Wallet, wallet, Token, token, owner, addr1, addr2;
+describe('Wallet contract', () => {
+  let Wallet, wallet, Token, token, owner;
   beforeEach(async () => {
-    Token = await ethers.getContractFactory("Token");
-    Wallet = await ethers.getContractFactory("Wallet");
+    Token = await ethers.getContractFactory('Token');
+    Wallet = await ethers.getContractFactory('Wallet');
 
     token = await Token.deploy();
     wallet = await Wallet.deploy();
-
-    [owner, addr1, addr2] = await ethers.getSigners();
+    await token.deployed();
+    [owner] = await ethers.getSigners();
   });
-  it("Should approve 500", async () => {
-    console.log('1');
-    await wallet.addToken(ethers.utils.formatBytes32String("TKN"), token.Address);
-    console.log('2');
+
+  it('Should deposit 100', async () => {
+    const tokenSymbol = ethers.utils.formatBytes32String(await token.symbol());
+    await wallet.addToken(tokenSymbol, token.address);
     await token.approve(wallet.address, 500);
-    console.log('3');
-    await wallet.deposit(100, ethers.utils.formatBytes32String("TKN"));
-    console.log('4');
+    await wallet.deposit(100, tokenSymbol);
+    const ownerBalance = await token.balanceOf(owner.address);
+    const balanceOfToken = await wallet.balances(owner.address, tokenSymbol);
+    expect(await token.totalSupply()-100).to.equal(ownerBalance);
+    expect(balanceOfToken === 100);
   });
 });
