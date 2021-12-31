59719edee631d25f37815e7a2e5a0b38d3e2e17e
matheus.custodio <matheus.c.conceicao@protonmail.com>
2021-12-31 13:23:02 -0300

    solhint

--- contracts/Dex.sol
+++ contracts/Dex.sol
@@ -5,28 +5,28 @@ pragma abicoder v2;
 import "./Wallet.sol";
 
 contract Dex is Wallet {
-    enum Type {
-        BUY,
-        SELL
-    }
-    struct Order {
-        uint256 id;
-        address trader;
-        Type orderType;
-        bytes32 ticker;
-        uint256 amount;
-        uint256 price;
-    }
+	enum Type {
+		BUY,
+		SELL
+	}
+	struct Order {
+		uint256 id;
+		address trader;
+		Type orderType;
+		bytes32 ticker;
+		uint256 amount;
+		uint256 price;
+	}
 
-    mapping(bytes32 => mapping(uint256 => Order[])) public orderBook;
+	mapping(bytes32 => mapping(uint256 => Order[])) public orderBook;
 
-    function getOrderBook(bytes32 ticker, Type orderType)
-        public
-        view
-        returns (Order[] memory)
-    {
-        return orderBook[ticker][uint256(orderType)];
-    }
+	function getOrderBook(bytes32 ticker, Type orderType)
+		public
+		view
+		returns (Order[] memory)
+	{
+		return orderBook[ticker][uint256(orderType)];
+	}
 
-    function createLimitOrder() public {}
+	function createLimitOrder() public {}
 }
--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -6,50 +6,54 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Wallet is Ownable {
-    using SafeMath for uint256;
+	using SafeMath for uint256;
 
-    struct Token {
-        bytes32 ticker;
-        address tokenAddress;
-    }
+	struct Token {
+		bytes32 ticker;
+		address tokenAddress;
+	}
 
-    mapping(bytes32 => Token) public tokenMapping;
-    bytes32[] public tokenList;
+	mapping(bytes32 => Token) public tokenMapping;
+	bytes32[] public tokenList;
 
-    mapping(address => mapping(bytes32 => uint256)) public balances;
+	mapping(address => mapping(bytes32 => uint256)) public balances;
 
-    modifier tokenExist(bytes32 ticker) {
-        require(
-            tokenMapping[ticker].tokenAddress != address(0),
-            "Token not added"
-        );
-        _;
-    }
+	modifier tokenExist(bytes32 ticker) {
+		require(
+			tokenMapping[ticker].tokenAddress != address(0),
+			"Token not added"
+		);
+		_;
+	}
 
-    function addToken(bytes32 ticker, address tokenAddress) external onlyOwner {
-        tokenMapping[ticker] = Token(ticker, tokenAddress);
-        tokenList.push(ticker);
-    }
+	function addToken(bytes32 ticker, address tokenAddress) external onlyOwner {
+		tokenMapping[ticker] = Token(ticker, tokenAddress);
+		tokenList.push(ticker);
+	}
 
-    function deposit(uint256 amount, bytes32 ticker) external tokenExist(ticker) {
-        IERC20(tokenMapping[ticker].tokenAddress).transferFrom(
-            msg.sender,
-            address(this),
-            amount
-        );
-        balances[msg.sender][ticker] = balances[msg.sender][ticker].add(amount);
-    }
+	function deposit(uint256 amount, bytes32 ticker)
+		external
+		tokenExist(ticker)
+	{
+		IERC20(tokenMapping[ticker].tokenAddress).transferFrom(
+			msg.sender,
+			address(this),
+			amount
+		);
+		balances[msg.sender][ticker] = balances[msg.sender][ticker].add(amount);
+	}
 
-    function withdraw(uint256 amount, bytes32 ticker)
-        external
-        tokenExist(ticker)
-    {
-        require(
-            balances[msg.sender][ticker] >= amount,
-            "Balance not sufficient"
-        );
+	function withdraw(uint256 amount, bytes32 ticker)
+		external
+		tokenExist(ticker)
+	{
+		require(
+			balances[msg.sender][ticker] >= amount,
+			"Balance not sufficient"
+		);
 
-        balances[msg.sender][ticker] = balances[msg.sender][ticker].sub(amount);
-        IERC20(tokenMapping[ticker].tokenAddress).transfer(msg.sender, amount);
-    }
+		balances[msg.sender][ticker] = balances[msg.sender][ticker].sub(amount);
+
+		IERC20(tokenMapping[ticker].tokenAddress).transfer(msg.sender, amount);
+	}
 }
