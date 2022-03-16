75f7449074784807a224f5c262898fbe4149e368
cha <matheus.c.conceicao@protonmail.com>
2022-03-16 12:49:30 -0300

    init

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+const navigation = [
+  { name: 'Product', href: '#' },
+  { name: 'Features', href: '#' },
+  { name: 'Marketplace', href: '#' },
+  { name: 'Company', href: '#' },
+];
+
+export default function Header() {
+  return (
+    <div className="w-full h-16 bg-slate-900 font-">
+      <div className="text-white">Header</div>
+    </div>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,6 @@
+import Header from '../components/Header';
+function HomePage() {
+  return <Header />;
+}
+
+export default HomePage;
