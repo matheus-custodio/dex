71cf77678b3a31415342a0a359df1472379c8c63
cha <matheus.c.conceicao@protonmail.com>
2022-03-18 13:09:53 -0300

    MORALIS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,19 @@
 import type { AppProps } from 'next/app';
+import { MoralisProvider } from 'react-moralis';
 import NavBar from '../components/NavBar';
 import '../styles/global.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="min-h-screen bg-slate-600">
-      <NavBar />
-      <Component {...pageProps} />
-    </div>
+    <MoralisProvider
+      appId={process.env.NEXT_PUBLIC_APP_ID ?? 'AppId Undefined'}
+      serverUrl={process.env.NEXT_PUBLIC_SERVER_URL ?? 'ServerUrl Undefined'}
+    >
+      <div className="min-h-screen bg-slate-600">
+        <NavBar />
+        <Component {...pageProps} />
+      </div>
+    </MoralisProvider>
   );
 }
 
